f6cfe0a707d3bccb2cb38fabcfcd99506d73c6bf
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-11-30 08:04:41 +0900

    added validation to login.dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({
     example: 'test@example.com',
     description: 'Registered user email address',
+    format: 'email',
   })
+  @IsEmail()
   email: string;
 
   @ApiProperty({
     example: '123456',
     description: 'User account password',
+    minLength: 6,
   })
+  @IsString()
+  @MinLength(6)
   password: string;
 }
